215c610fb92497ecc5267af655f742626d911e57
chopperlee <ChopperLee2011@users.noreply.github.com>
2015-04-03 17:51:38 +0800

    change to use factory design

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
-};
+module.exports = require('./lib/gitVFactory');
+
+// import strategy files;
+//var strategyPath = require('path').join(__dirname, "config");
+//
+//require('fs').readdirSync(strategyPath).forEach(function(file) {
+//    require("./strategyPath/" + file);
+//});
+
+//GitVendor.github = require('./lib/GitHub');
+//GitVendor.gitlab = require('./lib/GitLab');
+//
+//exports.githubFactory = require('./lib/githubFactory');
+//exports.gitlabFactory = require('./lib/gitlabFactory');
+
---
+++ test/test.define.js
@@ -0,0 +1,35 @@
+const gitVFactory = require('../'),
+    config = require('../config/index');
+var github = gitVFactory.create('github', config.github),
+    gitlab = gitVFactory.create('gitlab', config.gitlab);
+
+
+describe('git vendor api', function () {
+    it('defines github required operations', function (done) {
+        github.login.should.be.a.function;
+        github.getUser.should.be.a.function;
+        github.getOrg.should.be.a.function;
+        github.getRepo.should.be.a.function;
+        github.getOrgRepo.should.be.a.function;
+        github.getIssue.should.be.a.function;
+        github.getComment.should.be.a.function;
+        github.logout.should.be.a.function;
+
+        done();
+
+    });
+
+    it('defines gitlab required operations', function (done) {
+        gitlab.login.should.be.a.function;
+        gitlab.getUser.should.be.a.function;
+        gitlab.getOrg.should.be.a.function;
+        gitlab.getRepo.should.be.a.function;
+        gitlab.getOrgRepo.should.be.a.function;
+        gitlab.getIssue.should.be.a.function;
+        gitlab.getComment.should.be.a.function;
+        gitlab.logout.should.be.a.function;
+
+        done();
+
+    })
+});
